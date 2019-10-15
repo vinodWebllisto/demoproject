18fa071c1bada981a5ef284508d6c6cd3b6416b7
Vinod-Yadav <vinody242@>
2019-10-15 18:20:19 +0530

    Add project folder

---
+++ myapp/views.py
@@ -0,0 +1,6 @@
+fromdjango.http import httpresponse
+def welcome(request):
+    return httpresponse ("<html><body bgcolor=cyan><center><h1>welcome to vinodit</h1></center></body bgcolor body><html>")
+
+
+# Create your views here.
